159a5d78810334bc2cfa35fdee7284761cb0bc12
BhavaAG <86099856+BhavaAG@users.noreply.github.com>
2021-06-19 21:22:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 def application():
-    a = input("Enter the name:")
-    if(len(a)>=5):
-        print(a)
-    else:
+    try:
+        a = input("Enter the name:")
+        if(len(a)>=5):
+            print(a)
+        else:
+            raise Exception
+        b = int(input("Enter the age:"))
+        c = input("Enter the gender:")
+        d = int(input("Enter the salary:"))
+        e = input("Enter the state:")
+        f = input("Enter the city:")
+        if(len(f)>4):
+            print(f)
+        else:
+            print("Enter the valid city name")
+        print(b)
+        print(c)
+        print(d)
+        print(e)
+    except:
         print("Enter the valid name")
-    b = int(input("Enter the age:"))
-    c = input("Enter the gender:")
-    d = int(input("Enter the salary:"))
-    e = input("Enter the state:")
-    f = input("Enter the city:")
-    if(len(f)>4):
-        print(f)
-    else:
-        print("Enter the valid city name")
-    print(b)
-    print(c)
-    print(d)
-    print(e)
-
-application()
